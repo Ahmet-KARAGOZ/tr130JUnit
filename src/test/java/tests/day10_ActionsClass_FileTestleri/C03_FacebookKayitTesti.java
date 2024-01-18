8b46329edee7f1b933ba10adf6c5e9163258fd94
Ahmet-KARAGOZ <akaragoz5317@gmail.com>
2024-01-19 00:39:38 +0300

    18 Ocak 2024

--- src/test/java/tests/day10_ActionsClass_FileTestleri/C03_FacebookKayitTesti.java
+++ src/test/java/tests/day10_ActionsClass_FileTestleri/C03_FacebookKayitTesti.java
@@ -3,6 +3,7 @@ package tests.day10_ActionsClass_FileTestleri;
 import com.github.javafaker.Faker;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilities.ReusableMethods;
@@ -17,22 +18,42 @@ import utilities.TestBase;
 
 public class C03_FacebookKayitTesti extends TestBase {
 	@Test
-	public void testKayit(){
+	public void testKayit() {
 		driver.get("https://www.facebook.com");
 		WebElement yeniHesapTusu = driver.findElement(By.xpath("//*[text()='Yeni hesap oluştur']"));
 
 		yeniHesapTusu.click();
-		Actions actions =new Actions(driver);
+		Actions actions = new Actions(driver);
 		Faker faker = new Faker();
+		String fakeEmail = faker.internet().emailAddress();
+
+		WebElement firstNameKutusu = driver.findElement(By.xpath("//input[@*='firstname']"));
+		actions.click(firstNameKutusu)
+				.sendKeys(faker.name().firstName())
+				.sendKeys(Keys.TAB)
+				.sendKeys(faker.name().lastName())
+				.sendKeys(Keys.TAB)
+				.sendKeys(fakeEmail)
+				.sendKeys(Keys.TAB)
+				.sendKeys(fakeEmail)
+				.sendKeys(Keys.TAB)
+				.sendKeys(faker.internet().password())
+				.sendKeys(Keys.TAB)
+				.sendKeys(Keys.TAB)
+				.sendKeys("28")
+				.sendKeys(Keys.TAB)
+				.sendKeys("Şub")
+				.sendKeys(Keys.TAB)
+				.sendKeys("1985")
+				.sendKeys(Keys.TAB)
+				.sendKeys(Keys.TAB)
+				.sendKeys(Keys.ARROW_RIGHT)
+				.perform();
+
+		ReusableMethods.bekle(3);
+		driver.findElement(By.name("websubmit")).click();
+		ReusableMethods.bekle(3);
 
 
-
-
-
-
-
-
-
-		ReusableMethods.bekle(2);
 	}
 }
